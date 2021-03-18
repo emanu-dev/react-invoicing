01292bdb4b9ee33c674f17b14d65836d24860439
Emanuel Prado <emanu-dev@DESKTOP-JJOEB1K.localdomain>
2021-03-18 20:19:59 -0300

    refactor: remove angular code
    Remove all angular code and replace with a React Strucutre

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-import { Provider } from 'react-redux';
-
-import store from './store';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+reportWebVitals();
