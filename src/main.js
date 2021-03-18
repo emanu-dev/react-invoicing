fecbc29c27b8bcd59a28a8bea9b841cb9b4e996e
Emanuel Prado <emanu-dev@DESKTOP-JJOEB1K.localdomain>
2021-03-17 21:00:14 -0300

    build: update build script
    Update the build script and main.js location for a smoother development enviroment, allowing for
    changes in react components to be watched, also triggering browserify for changes

---
+++ src/main.js
@@ -0,0 +1,225 @@
+import angular from 'angular';
+import { react2angular } from 'react2angular';
+
+import Logo from './Components/Logo';
+import InfoInput from './Components/InfoInput/Container';
+import ItemTable from './Components/ItemTable/Container';
+
+import store from './store';
+
+angular.module('invoicing', [])
+	.constant('store', store)
+	.component('logo', react2angular(Logo, ['width']))
+	.component('infoInput', react2angular(InfoInput, ['label', 'type', 'id', 'info', 'name']))
+	.component('itemTable', react2angular(ItemTable, []))
+
+	// The default logo for the invoice
+	// .constant('DEFAULT_LOGO', 'images/metaware_logo.png')
+
+	// The invoice displayed when the user first uses the app
+	// .constant('DEFAULT_INVOICE', {
+	// 	tax: 13.00,
+	// 	invoice_number: 10,
+	// 	customer_info: {
+	// 		name: 'Mr. John Doe',
+	// 		web_link: 'John Doe Designs Inc.',
+	// 		address1: '1 Infinite Loop',
+	// 		address2: 'Cupertino, California, US',
+	// 		postal: '90210'
+	// 	},
+	// 	company_info: {
+	// 		name: 'Metaware Labs',
+	// 		web_link: 'www.metawarelabs.com',
+	// 		address1: '123 Yonge Street',
+	// 		address2: 'Toronto, ON, Canada',
+	// 		postal: 'M5S 1B6'
+	// 	},
+	// 	items: [
+	// 		{ qty: 10, description: 'Gadget', cost: 9.95 }
+	// 	]
+	// })//
+
+	// Service for accessing local storage
+	.service('LocalStorage', [function () {
+
+		var Service = {};
+
+		// Returns true if there is a logo stored
+		var hasLogo = function () {
+			return !!localStorage['logo'];
+		};//
+
+		// Returns a stored logo (false if none is stored)
+		Service.getLogo = function () {
+			if (hasLogo()) {
+				return localStorage['logo'];
+			} else {
+				return false;
+			}
+		};
+
+		Service.setLogo = function (logo) {
+			localStorage['logo'] = logo;
+		};//
+
+		// Checks to see if an invoice is stored
+		var hasInvoice = function () {
+			return !(localStorage['invoice'] == '' || localStorage['invoice'] == null);
+		};
+
+		// Returns a stored invoice (false if none is stored)
+		Service.getInvoice = function () {
+			if (hasInvoice()) {
+				return JSON.parse(localStorage['invoice']);
+			} else {
+				return false;
+			}
+		};
+
+		Service.setInvoice = function (invoice) {
+			localStorage['invoice'] = JSON.stringify(invoice);
+		};
+
+		// Clears a stored logo
+		Service.clearLogo = function () {
+			localStorage['logo'] = '';
+		};
+
+		// Clears a stored invoice
+		Service.clearinvoice = function () {
+			localStorage['invoice'] = '';
+		};
+
+		// Clears all local storage
+		Service.clear = function () {
+			localStorage['invoice'] = '';
+			Service.clearLogo();
+		};
+
+		return Service;
+
+	}])
+
+	.service('Currency', [function () {
+
+		var service = {};
+
+		service.all = function () {
+			return [
+				{
+					name: 'British Pound (£)',
+					symbol: '£'
+				},
+				{
+					name: 'Canadian Dollar ($)',
+					symbol: 'CAD $ '
+				},
+				{
+					name: 'Euro (€)',
+					symbol: '€'
+				},
+				{
+					name: 'Indian Rupee (₹)',
+					symbol: '₹'
+				},
+				{
+					name: 'Norwegian krone (kr)',
+					symbol: 'kr '
+				},
+				{
+					name: 'US Dollar ($)',
+					symbol: '$'
+				}
+			]
+		}
+
+		return service;
+
+	}])
+
+	// Main application controller
+	.controller('InvoiceCtrl', ['$scope', '$http', 'LocalStorage', 'Currency',
+		function ($scope, $http, LocalStorage, Currency) {//
+
+			// Set defaults
+			$scope.currencySymbol = '$';
+			// $scope.logoRemoved = false;
+			$scope.printMode = false;
+
+			// (function init() {
+			// 	// Attempt to load invoice from local storage
+			// 	!function () {
+			// 		var invoice = LocalStorage.getInvoice();
+			// 		$scope.invoice = invoice ? invoice : DEFAULT_INVOICE;
+			// 	}();
+
+			// 	// Set logo to the one from local storage or use default
+			// 	!function () {
+			// 		var logo = LocalStorage.getLogo();
+			// 		$scope.logo = logo ? logo : DEFAULT_LOGO;
+			// 	}();
+
+			$scope.availableCurrencies = Currency.all();
+
+			// })()
+			// // Adds an item to the invoice's items
+			// $scope.addItem = function () {
+			// 	$scope.invoice.items.push({ qty: 0, cost: 0, description: "" });
+			// }
+
+			// $scope.printInfo = function () {
+			// 	window.print();
+			// };
+
+			// // Remotes an item from the invoice
+			// $scope.removeItem = function (item) {
+			// 	$scope.invoice.items.splice($scope.invoice.items.indexOf(item), 1);
+			// };
+
+			// // Calculates the sub total of the invoice
+			// $scope.invoiceSubTotal = function () {
+			// 	var total = 0.00;
+			// 	angular.forEach($scope.invoice.items, function (item, key) {
+			// 		total += (item.qty * item.cost);
+			// 	});
+			// 	return total;
+			// };
+
+			// // Calculates the tax of the invoice
+			// $scope.calculateTax = function () {
+			// 	return (($scope.invoice.tax * $scope.invoiceSubTotal()) / 100);
+			// };
+
+			// // Calculates the grand total of the invoice
+			// $scope.calculateGrandTotal = function () {
+			// 	saveInvoice();
+			// 	return $scope.calculateTax() + $scope.invoiceSubTotal();
+			// };
+
+			// // Clears the local storage
+			// $scope.clearLocalStorage = function () {
+			// 	var confirmClear = confirm('Are you sure you would like to clear the invoice?');
+			// 	if (confirmClear) {
+			// 		LocalStorage.clear();
+			// 		setInvoice(DEFAULT_INVOICE);
+			// 	}
+			// };
+
+			// // Sets the current invoice to the given one
+			// var setInvoice = function (invoice) {
+			// 	$scope.invoice = invoice;
+			// 	saveInvoice();
+			// };
+
+			// // Saves the invoice in local storage
+			// var saveInvoice = function () {
+			// 	LocalStorage.setInvoice($scope.invoice);
+			// };
+
+			// Runs on document.ready
+			angular.element(document).ready(function () {
+				// Set focus
+				document.getElementById('invoice-number').focus();
+			});
+
+		}])
