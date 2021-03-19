5398f0e4c5bcd08913e1a959fda4d9246bbff770
Emanuel Prado <emanu-dev@DESKTOP-JJOEB1K.localdomain>
2021-03-19 11:28:29 -0300

    test: update app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+
+import store from './store';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders INVOICE', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const printModeButtonOn = screen.getByText(/Turn On Print Mode/i);
+  expect(printModeButtonOn).toBeInTheDocument();
 });
